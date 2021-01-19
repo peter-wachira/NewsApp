32754822c5ab017b90a952ca48377ceeea45f180
PeteHack <pwachira900@gmail.com>
2021-01-19 11:38:56 +0300

    add view model declaration

--- app/src/main/java/com/droid/newsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/droid/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,8 +1,20 @@
 package com.droid.newsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.droid.newsapp.R
+import com.droid.newsapp.ui.ui.NewsActivity
+import com.droid.newsapp.ui.ui.NewsViewModel
 
 class SearchNewsFragment  : Fragment(R.layout.fragment_search_news){
+
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = (activity as NewsActivity).viewModel
+    }
 
 }
