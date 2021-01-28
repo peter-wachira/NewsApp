3b93dc01378d4835a2a3829b7d058dda68875b4c
PeteHack <pwachira900@gmail.com>
2021-01-28 11:10:37 +0300

    add method to handle breaking news response

--- app/src/main/java/com/droid/newsapp/ui/ui/NewsViewModel.kt
+++ app/src/main/java/com/droid/newsapp/ui/ui/NewsViewModel.kt
@@ -1,10 +1,39 @@
 package com.droid.newsapp.ui.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.droid.newsapp.ui.models.NewsResponse
 import com.droid.newsapp.ui.repository.NewsRepository
+import com.droid.newsapp.ui.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
 val newsRepository : NewsRepository
 ) : ViewModel() {
 
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
+
+
+    init {
+        getBreakingNews("US")
+    }
+
+    /*Method to notify fragments whenever Live Data is updates */
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
