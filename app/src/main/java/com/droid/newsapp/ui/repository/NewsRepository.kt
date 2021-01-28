b9de2303de777141b414ed285d20261030ead0ce
PeteHack <pwachira900@gmail.com>
2021-01-28 11:08:29 +0300

    add getBreakingNews method

--- app/src/main/java/com/droid/newsapp/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/droid/newsapp/ui/repository/NewsRepository.kt
@@ -1,9 +1,10 @@
 package com.droid.newsapp.ui.repository
 
+import com.droid.newsapp.ui.api.RetrofitInstance
 import com.droid.newsapp.ui.db.ArticleDatabase
 
 class NewsRepository(
     val db: ArticleDatabase
 ){
-
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) = RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
