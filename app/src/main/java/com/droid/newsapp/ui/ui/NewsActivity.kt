423d2e2ddf7b172899a5f2d45bcb01efd55fbdb9
PeteHack <pwachira900@gmail.com>
2021-01-28 11:05:33 +0300

    initialize view model

--- app/src/main/java/com/droid/newsapp/ui/ui/NewsActivity.kt
+++ app/src/main/java/com/droid/newsapp/ui/ui/NewsActivity.kt
@@ -1,17 +1,26 @@
-package com.droid.newsapp.ui
+package com.droid.newsapp.ui.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.droid.newsapp.R
+import com.droid.newsapp.ui.db.ArticleDatabase
+import com.droid.newsapp.ui.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
