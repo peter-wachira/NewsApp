993d98da7de16bd9c55b72a47b58e87385570df8
PeteHack <pwachira900@gmail.com>
2021-02-05 13:15:35 +0300

    update api jey

--- app/src/main/java/com/droid/newsapp/ui/util/Constants.kt
+++ app/src/main/java/com/droid/newsapp/ui/util/Constants.kt
@@ -3,7 +3,7 @@ package com.droid.newsapp.ui.util
 class Constants {
 
     companion object{
-        const val API_KEY = "6953a7421bb749c589e687746c85eee8"
+        const val API_KEY = "b1893f14fcbf4ca9bcb7146de86f3a1c"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY  = 500L
     }
