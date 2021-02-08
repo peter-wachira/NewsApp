19357ab522970a962bdfc1c0585b0dbf16a6efaa
PeteHack <pwachira900@gmail.com>
2021-02-08 09:33:06 +0300

    Update token

--- app/src/main/java/com/droid/newsapp/ui/util/Constants.kt
+++ app/src/main/java/com/droid/newsapp/ui/util/Constants.kt
@@ -3,7 +3,7 @@ package com.droid.newsapp.ui.util
 class Constants {
 
     companion object{
-        const val API_KEY = "b1893f14fcbf4ca9bcb7146de86f3a1c"
+        const val API_KEY = "b1893f14fcbf4ca9bcb71846de86f3a1c"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY  = 500L
     }
