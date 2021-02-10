8d6f2780c237eb4012716db7553d2c853f9099e9
PeteHack <pwachira900@gmail.com>
2021-02-10 12:56:43 +0300

    Add method to upsert and delete Articles

--- app/src/main/java/com/droid/newsapp/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/droid/newsapp/ui/repository/NewsRepository.kt
@@ -2,11 +2,19 @@ package com.droid.newsapp.ui.repository
 
 import com.droid.newsapp.ui.api.RetrofitInstance
 import com.droid.newsapp.ui.db.ArticleDatabase
+import com.droid.newsapp.ui.models.Article
 
 class NewsRepository(
     val db: ArticleDatabase
-){
+) {
     suspend fun getBreakingNews(countryCode: String, pageNumber: Int) = RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 
-    suspend fun searchNews(searchQuery : String, pageNumber: Int)= RetrofitInstance.api.searchForNews(searchQuery,pageNumber)
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) = RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticles(article: Article) = db.getArticleDao().deleteArticle(article)
+
 }
